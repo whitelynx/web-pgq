5efd0d9d576e2546c027c334596a1e9e9ff5b74c
David H. Bronke <whitelynx@gmail.com>
2014-09-30 00:17:18 -0500

    Fixes for queueDigest.

--- static/js/services/queue-digest.js
+++ static/js/services/queue-digest.js
@@ -1,17 +1,47 @@
 /* global angular: true */
 
 angular.module('webPGQ.services')
-    .service('queueDigest', ['$timeout', function($timeout)
+    .service('queueDigest', ['$exceptionHandler', '$q', '$timeout', function($exceptionHandler, $q, $timeout)
     {
         var currentTimer;
         var currentTimerTargetTime;
+        var queuedFuncs = [];
 
         function runDigest()
         {
+            var funcs = queuedFuncs;
+            queuedFuncs = [];
+
+            funcs.forEach(function(call) { call(); });
         } // end runDigest
+
+        function wrap(func)
+        {
+            var deferred = $q.defer();
+
+            return {
+                call: function()
+                {
+                    try
+                    {
+                        deferred.resolve(func());
+                    }
+                    catch(exc)
+                    {
+                        deferred.reject();
+                        $exceptionHandler(exc);
+                    } // end try
+                }, // end call
+                promise: deferred.promise
+            };
+        } // end wrap
 
         return function queueDigest(func, maxDelay)
         {
+            var wrapped = wrap(func);
+
+            queuedFuncs.push(wrapped.call);
+
             var targetTime = Date.now() + maxDelay;
 
             if(currentTimer)
@@ -19,7 +49,7 @@ angular.module('webPGQ.services')
                 if(currentTimerTargetTime < targetTime)
                 {
                     // Current timer will expire before maxDelay is up; leave that timer alone.
-                    return currentTimer.then(func);
+                    return wrapped.promise;
                 } // end if
 
                 $timeout.cancel(currentTimer);
@@ -28,6 +58,6 @@ angular.module('webPGQ.services')
             currentTimerTargetTime = targetTime;
             currentTimer = $timeout(runDigest, maxDelay);
 
-            return currentTimer.then(func);
+            return wrapped.promise;
         }; // end promise
     }]);
