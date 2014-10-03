0328402ead23833765e6daa6ca67911905ac1cdd
David H. Bronke <whitelynx@gmail.com>
2014-10-03 17:25:42 -0500

    Added 'await' service.

---
+++ static/js/services/await.js
@@ -0,0 +1,26 @@
+/* global angular: true */
+
+angular.module('webPGQ.services')
+    .service('await', ['$exceptionHandler', '$q', function($exceptionHandler, $q)
+    {
+        return function await(func)
+        {
+            var deferred = $q.defer();
+
+            return {
+                trigger: function()
+                {
+                    try
+                    {
+                        deferred.resolve(func());
+                    }
+                    catch(exc)
+                    {
+                        deferred.reject(exc);
+                        $exceptionHandler(exc);
+                    } // end try
+                }, // end trigger
+                promise: deferred.promise
+            };
+        }; // end await
+    }]);
