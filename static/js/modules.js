95005358619831e3a941d3712d930b4c940e825f
David H. Bronke <dbronke@armt.com>
2014-10-15 16:25:21 -0500

    Added fade in/out animations to the banner message list.

--- static/js/modules.js
+++ static/js/modules.js
@@ -17,6 +17,6 @@ angular.module('dagreD3', ['d3']).factory('dagreD3', function($window) { return 
 /////////////////////////////
 //// Application Modules ////
 
-angular.module('webPGQ', ['jQuery', 'ui.ace', 'webPGQ.directives', 'webPGQ.services']);
+angular.module('webPGQ', ['jQuery', 'ui.ace', 'ngAnimate', 'webPGQ.directives', 'webPGQ.services']);
 angular.module('webPGQ.services', ['unisocket', 'd3', 'dagreD3', 'rt.eventemitter']);
 angular.module('webPGQ.directives', ['webPGQ.services']);
