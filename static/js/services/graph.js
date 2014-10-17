c4cbc08c225ff14141c2078034291d2da5ea82b2
David H. Bronke <whitelynx@gmail.com>
2014-10-17 10:47:34 -0500

    Added the 'References' metadata property to graph nodes, so we can display references to other nodes.

--- static/js/services/graph.js
+++ static/js/services/graph.js
@@ -23,19 +23,56 @@ angular.module('webPGQ.services')
             'Window Agg'
         ];
 
+        var nodeRefRE = /\$\d+\b/g;
+
+        var nodeIDsByRef = {};
+
         var graphService = {
             nodesFromPlan: function(graph, plan)
             {
                 maxTotalCost = Math.max(maxTotalCost, plan['Total Cost']);
 
                 var metadata = {};
+                var thisNodeRef;
+                var references = [];
                 for(var key in plan)
                 {
                     if(key != 'Plans')
                     {
-                        metadata[key] = plan[key];
+                        var val = plan[key];
+                        metadata[key] = val;
+
+                        var match;
+                        if(key == 'Subplan Name')
+                        {
+                            while((match = nodeRefRE.exec(val)) !== null)
+                            {
+                                thisNodeRef = match[0];
+                            } // end while
+                        }
+                        else
+                        {
+                            while((match = nodeRefRE.exec(val)) !== null)
+                            {
+                                references.push(match[0]);
+                            } // end while
+                        } // end if
                     } // end if
                 } // end for
+
+                if(references.length > 0)
+                {
+                    Object.defineProperty(metadata, 'References', {
+                        enumerable: true,
+                        get: function()
+                        {
+                            return references.map(function(ref)
+                            {
+                                return {name: ref, id: nodeIDsByRef[ref]};
+                            });
+                        }
+                    });
+                } // end if
 
                 var useDef;
                 if(nodeTypeIcons.indexOf(plan['Node Type']) != -1)
@@ -51,12 +88,17 @@ angular.module('webPGQ.services')
                     iconURL: 'icons/' + plan['Node Type'] + '.svg'
                 });
 
+                if(thisNodeRef)
+                {
+                    nodeIDsByRef[thisNodeRef] = thisNodeID;
+                } // end if
+
                 (plan.Plans || [])
                     .forEach(function(childPlan)
                     {
                         var childInfo = graphService.nodesFromPlan(graph, childPlan);
 
-                        // Calculate an edge size of between 1 and 40 pixels, based on percentage of the maximum total cost.
+                        // Calculate an edge size between 1 and 40 pixels, based on percentage of the max total cost.
                         var edgeSize = (childInfo['Total Cost'] / maxTotalCost * 39) + 1;
 
                         graph.addEdge(null, childInfo.id, thisNodeID, {
