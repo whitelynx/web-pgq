653215a48c4159e9c36eeae8649cc82d0c3ba643
David H. Bronke <whitelynx@gmail.com>
2014-10-24 12:07:13 -0500

    Lots of fixes to result-value drop-down.
    - Use common editor config's onLoad() to reduce code duplication.
    - Correctly set height of dropdown. (limited to 20 lines)
    - Resize editor only when appropriate.
    - Clear editor selection after setting value and height. (for some reason it starts with everything selected)
    - Indent stringified JSON using tabs. (so Ace displays indent at the size of the global indent setting)
    - Set dropdown editor mode based on detected data type. (JSON/XML)
    - Fixed handling of `text` field types.
    - Probably more random fixes and such.

--- static/js/directives/result-value.js
+++ static/js/directives/result-value.js
@@ -1,18 +1,39 @@
 /* global angular: true */
 
 angular.module('webPGQ.directives')
-    .directive('resultValue', ['$filter', '$timeout', '$', 'queueDigest', function($filter, $timeout, $, queueDigest)
+    .directive('resultValue', ['$window', '$', 'queueDigest', function($window, $, queueDigest)
     {
         var firstLineRE = /^([^\n]*)\n/;
+        var newlineRE = /\n/g;
 
-        var dropdownSettings = { on: 'hover', delay: { show: 10 } };
+        var geomFieldNameRE = /geo|shape|wk[bt]|[gk]ml|svg|x3d/i;
+
+        var maybeJSONRE = /^\s*(?:\{[^]*\}|\[[^]*\]|".*"|\d+(?:\.\d*)?|\.\d+|null)\s*$/;
+        var maybeEWKTRE = /^\s*(?:SRID\s*=\s*\d+\s*;\s*)?\w+\s*\([^]*\)\s*$/;
+        var maybeXMLRE = /^\s*<[a-z!?][^]*>\s*$/i;
 
         function link(scope, element)//, attrs)
         {
-            console.log("result-value directive link.");
-            scope.editorConfig = angular.extend({ mode: 'json' }, scope.aceConfig || {}, {onLoad: onAceLoad});
+            scope.editorConfig = angular.extend({}, scope.aceConfig || {}, { onLoad: onAceLoad });
 
-            var editor;
+            var editor, needsResize = true;
+
+            var dropdownSettings = {
+                on: 'hover',
+                delay: { show: 10 },
+                transition: 'fade',
+                onShow: function()
+                {
+                    $window.setTimeout(function()
+                    {
+                        if(editor && needsResize)
+                        {
+                            editor.resize();
+                        } // end if
+                    }, 0);
+                } // end onShow
+            }; // end dropdownSettings
+
             scope.$watch('resultValue', function(value)
             {
                 updateFirstLine(value);
@@ -23,76 +44,38 @@ angular.module('webPGQ.directives')
                 updateFirstLine(scope.resultValue);
             }); // end scope.$watch callback
 
-            scope.initMenu = function()
+            scope.$watch('firstLine', function()
             {
-                $('.ui.dropdown', element).dropdown(angular.extend(
-                    {
-                        onShow: function()
-                        {
-                            if(editor)
-                            {
-                                editor.resize();
-                            } // end if
-                        } // end onShow
-                    },
-                    dropdownSettings
-                ));
-            };
+                $window.setTimeout(function()
+                {
+                    element.dropdown(dropdownSettings);
+                }, 0);
+            });
 
             function onAceLoad(_editor)
             {
                 editor = _editor;
-                console.log("ACE editor loaded:", editor);
-
-                // Options
-                var session = editor.getSession();
-                session.setTabSize(2);
-                session.setUseSoftTabs(true);
-
-                var editorElem = $(editor.renderer.getContainerElement());
-                var scrollbars = $('.ace_scrollbar', editorElem)
-                    .css('overflow', 'hidden');
-
-                scrollbars.filter('.ace_scrollbar-v')
-                    .perfectScrollbar({ includePadding: true, minScrollbarLength: 12, suppressScrollX: true });
-                scrollbars.filter('.ace_scrollbar-h')
-                    .perfectScrollbar({ includePadding: true, minScrollbarLength: 12, suppressScrollY: true });
-
-                session.on('change', function()
-                {
-                    scrollbars.perfectScrollbar('update');
-                });
-
-                editorElem.hover(
-                    function() { scrollbars.addClass('hover'); },
-                    function() { scrollbars.removeClass('hover'); }
-                );
+                scope.aceConfig.onLoad(editor);
 
                 queueDigest(function()
                 {
-                    var value = scope.resultValue;
-                    editor.setValue($filter('json')(value));
-                    updateFirstLine(value);
+                    updateFirstLine(scope.resultValue);
                 }, 0);
-
-                return { editor: editorElem, scrollbars: scrollbars };
             } // end onAceLoad
 
             function updateFirstLine(value)
             {
-                console.log("result-value~updateFirstLine: value =", value);
-                console.log("result-value~updateFirstLine: scope.resultFieldType =", scope.resultFieldType);
+                var fieldType = scope.resultFieldType, fieldName = scope.resultFieldName;
+                var displayValue = value, firstLine = value, editorMode;
 
-                var displayValue = value, firstLine = value;
-
-                switch(scope.resultFieldType)
+                switch(fieldType)
                 {
                     case 'json':
-                        element.addClass("hoverable ui dropdown");
                         firstLine = JSON.stringify(value);
-                        displayValue = $filter('json')(value);
+                        displayValue = JSON.stringify(value, null, '\t');
+                        editorMode = 'json';
                         /* falls through */
-                    case 'string':
+                    case 'text':
                         var match = firstLineRE.exec(firstLine);
                         if(match)
                         {
@@ -103,19 +86,60 @@ angular.module('webPGQ.directives')
                         {
                             firstLine = firstLine.slice(0, 50) + '\u2026';
                         } // end if
-
-                        if(firstLine != value)
-                        {
-                            scope.firstLine = firstLine;
-                        } // end if
                         break;
                     default:
                         // Nothing.
                 } // end switch
 
+                var looksLikeGeomField = geomFieldNameRE.test(fieldName);
+                if(fieldType == 'text')
+                {
+                    // Try to detect what sort of data may be in this string.
+                    if(maybeJSONRE.test(value))
+                    {
+                        editorMode = 'json';
+                    }
+                    else if(maybeEWKTRE.test(value))
+                    {
+                        // Looks like [E]WKT...
+                        //editorMode = 'ewkt';
+                        // But, until we write an EWKT mode for Ace...
+                        editorMode = 'text';
+                    }
+                    else if(maybeXMLRE.test(value))
+                    {
+                        editorMode = 'xml';
+                        /*
+                        if(looksLikeGeomField)
+                        {
+                            // This looks like a geometry field; it may be GML, KML, or X3D.
+                        } // end if
+                        */
+                    }
+                    else if(looksLikeGeomField)
+                    {
+                        // This looks like a geometry field, but doesn't look like JSON, [E]WKT, or XML.
+                        // It may be HEX[E]WKB, SVG path, GeoHash, lat/lon text, etc.
+                    } // end if
+                } // end if
+
+                if(firstLine != value)
+                {
+                    element.addClass("hoverable ui dropdown");
+                    scope.firstLine = firstLine;
+                } // end if
+
                 if(editor)
                 {
+                    editor.getSession().setMode('ace/mode/' + (editorMode || 'text'));
                     editor.setValue(displayValue);
+
+                    var newlines = displayValue.match(newlineRE) || { length: 0 };
+                    var lineCount = Math.max(newlines.length + 1, 20);
+                    needsResize = (scope.lineCount != lineCount);
+                    scope.lineCount = lineCount;
+
+                    editor.clearSelection();
                 } // end if
             } // end updateFirstLine
         } // end link
@@ -124,6 +148,7 @@ angular.module('webPGQ.directives')
             scope: {
                 resultValue: '=',
                 resultFieldType: '=',
+                resultFieldName: '=',
                 aceConfig: '='
             },
             templateUrl: '/js/directives/result-value.html',
