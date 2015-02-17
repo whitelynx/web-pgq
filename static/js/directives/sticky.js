7456c1c1a60ed00faefec1faa5fe3f3d40fa0c0d
David H. Bronke <dbronke@armt.com>
2015-02-17 17:11:53 -0600

    Implemented the <sticky> directive without using semantic-ui's sticky.

--- static/js/directives/sticky.js
+++ static/js/directives/sticky.js
@@ -1,25 +1,85 @@
 /* jshint browser:true, globalstrict:true */
-/* global angular:true, console:true */
+/* global angular:true */
 "use strict";
 
 angular.module('webPGQ.directives')
-    .directive('sticky', ['$window', function($window)
+    .directive('sticky', ['$window', '$', function($window, $)
     {
         function link(scope, element)//, attrs)
         {
-            element.sticky({
-                context: scope.context,
-                scrollContext: scope.scrollContext,
-                pushing: scope.pushing
-            });
+            var placeholder = $('<div></div>')
+                .addClass('sticky-placeholder')
+                .width(element.width)
+                .height(element.height)
+                .insertBefore(element);
+
+            var context = $(scope.context);
+            var isFixed = false;
+
+            function makeFixed()
+            {
+                if(isFixed) { return; }
+
+                var contextOffset = context.offset();
+                var elementOffset = element.offset();
+
+                placeholder
+                    .width(element.outerWidth())
+                    .height(element.outerHeight());
+
+                element
+                    .css({
+                        top: contextOffset.top,
+                        left: elementOffset.left,
+                        right: elementOffset.left
+                    })
+                    .addClass('fixed');
+
+                isFixed = true;
+            } // end makeFixed
+
+            function makeStatic()
+            {
+                if(!isFixed) { return; }
+
+                var placeholderOffset = placeholder.offset();
+
+                element
+                    .css({ top: placeholderOffset.top, left: placeholderOffset.left, right: '' })
+                    .removeClass('fixed');
+
+                placeholder
+                    .height(0);
+
+                isFixed = false;
+            } // end makeStatic
+
+            makeStatic();
 
             scope.$on('ResultsTabChanged', function()
             {
-                console.log('sticky got TabChanged; refreshing.');
-                $window.setTimeout(function()
+                var wasFixed = isFixed;
+                makeStatic();
+
+                if(wasFixed)
                 {
-                    element.sticky('refresh');
-                }, 0);
+                    makeFixed();
+                } // end if
+            });
+
+            context.scroll(function()
+            {
+                var contextOffset = context.offset();
+                var placeholderOffset = placeholder.offset();
+
+                if(placeholderOffset.top < contextOffset.top)
+                {
+                    makeFixed();
+                }
+                else
+                {
+                    makeStatic();
+                } // end if
             });
         } // end link
 
