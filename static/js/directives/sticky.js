8e112b79940b6328b27343e90d9cd23e90d7aa75
David H. Bronke <dbronke@armt.com>
2015-02-19 09:36:34 -0600

    Extend background and border of fixed <sticky> headers, and put scrollbars above them.

--- static/js/directives/sticky.js
+++ static/js/directives/sticky.js
@@ -14,6 +14,7 @@ angular.module('webPGQ.directives')
                 .insertBefore(element);
 
             var context = $(scope.context);
+            context.addClass('sticky-container');
             var isFixed = false;
 
             function makeFixed()
