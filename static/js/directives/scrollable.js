5772c79e8d9d01068b0d371cd5a790bfbffef7c3
David H. Bronke <whitelynx@gmail.com>
2015-03-05 17:44:11 -0600

    Added the `scrollable` attribute directive.

---
+++ static/js/directives/scrollable.js
@@ -0,0 +1,34 @@
+/* jshint browser:true, globalstrict:true */
+/* global angular:true */
+"use strict";
+
+angular.module('webPGQ.directives')
+    .directive('scrollable', ['$rootScope', '_', function($rootScope, _)
+    {
+        function link(scope, element)//, attrs)
+        {
+            element.perfectScrollbar(_.defaults({}, scope.options, $rootScope.scrollableDefaults));
+
+            scope.$on('windowResized', update);
+            scope.$on('bgSplitter.resizeFinished', update);
+            scope.$on('contentResized', update);
+
+            function update(ev)
+            {
+                element.perfectScrollbar('update');
+
+                if(ev.stopPropagation)
+                {
+                    ev.stopPropagation();
+                } // end if
+            } // end update
+        } // end link
+
+        return {
+            restrict: 'A',
+            scope: {
+                options: '=scrollable'
+            },
+            link: link
+        };
+    }]);
