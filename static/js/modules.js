6a4f78d713ee1bc93ba8c1a11fec0da0dd77a06b
David H. Bronke <whitelynx@gmail.com>
2015-03-16 12:17:30 -0500

    Added angular-recursion, needed in order to implement the `inspect=` directive.

--- static/js/modules.js
+++ static/js/modules.js
@@ -25,5 +25,5 @@ angular.module('ol', []).factory('ol', function($window) { return $window.ol; })
 angular.module('webPGQ', ['jQuery', 'hljs', 'lodash', 'ol', 'ngAnimate', 'ngCookies', 'openlayers-directive',
     'bgDirectives', 'webPGQ.directives', 'webPGQ.filters', 'webPGQ.services']);
 angular.module('webPGQ.services', ['unisocket', 'd3', 'dagreD3', 'rt.eventemitter']);
-angular.module('webPGQ.directives', ['ui.ace', 'hljs', 'webPGQ.services']);
+angular.module('webPGQ.directives', ['ui.ace', 'hljs', 'RecursionHelper', 'webPGQ.services']);
 angular.module('webPGQ.filters', []);
