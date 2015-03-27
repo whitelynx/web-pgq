c99f436792e5d703a1b005be3f16ac227ebbec7a
David H. Bronke <whitelynx@gmail.com>
2015-03-27 18:17:49 -0500

    Tweaked stuff in inspect= directive.

--- static/js/directives/inspect.js
+++ static/js/directives/inspect.js
@@ -3,8 +3,35 @@
 "use strict";
 
 angular.module('webPGQ.directives')
-    .directive('inspect', ['RecursionHelper', 'detect', function(RecursionHelper, detect)
+    .directive('inspect', ['$sce', '_', 'RecursionHelper', 'detect', function($sce, _, RecursionHelper, detect)
     {
+        function hasInspect(value)
+        {
+            return value && _.isFunction(value.htmlInspect);
+        } // end hasInspect
+
+        function htmlInspect(value)
+        {
+            return $sce.trustAsHtml(value.htmlInspect());
+        } // end hasInspect
+
+        function valueType(value)
+        {
+            if(detect.isArray(value))
+            {
+                return 'array';
+            }
+            else if(_.isNumber(value) || _.isString(value) || _.isRegExp(value) ||
+                _.isNull(value) || _.isUndefined(value))
+            {
+                return 'primitive';
+            }
+            else if(detect.isObject(value))
+            {
+                return 'object';
+            } // end if
+        } // end valueType
+
         return {
             restrict: 'A',
             scope: {
@@ -18,10 +45,19 @@ angular.module('webPGQ.directives')
                 return RecursionHelper.compile(element, function(scope)
                 {
                     scope.detect = detect;
+                    scope.hasInspect = hasInspect;
+                    scope.htmlInspect = htmlInspect;
 
                     scope.$watch('value', function(value)
                     {
                         element.toggleClass('multi-line', detect.isMultiLine(value));
+
+                        if(scope.valueType)
+                        {
+                            element.removeClass(scope.valueType);
+                        } // end if
+                        scope.valueType = valueType(scope.value);
+                        element.addClass(scope.valueType);
                     });
                 });
             },
