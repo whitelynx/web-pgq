8606261a5cf65e5228f5672d91bc0565b9fb2983
David H. Bronke <whitelynx@gmail.com>
2015-04-17 16:47:57 -0500

    Added support for `dropdown-action` callback.

--- static/js/directives/dropdown.js
+++ static/js/directives/dropdown.js
@@ -1,22 +1,58 @@
 /* jshint browser:true, globalstrict:true */
-/* global angular:true */
+/* global angular:true, console:true */
 "use strict";
 
 angular.module('webPGQ.directives')
-    .directive('dropdown', function()
+    .directive('dropdown', ['_', function(_)
     {
         function link(scope, element)//, attrs)
         {
-            element
-                .addClass('ui dropdown')
-                .dropdown(scope.dropdownOptions);
+            element.addClass('ui dropdown');
+
+            var options = scope.dropdownOptions ? _.clone(scope.dropdownOptions) : {};
+
+            function updateCallbacks()
+            {
+                var action = scope.dropdownAction;
+                if(action)
+                {
+                    options.action = function(text, value)
+                    {
+                        try
+                        {
+                            action({
+                                text: text,
+                                value: value,
+                                dropdown: {
+                                    hide: function() { element.dropdown('hide'); }
+                                }
+                            });
+                        }
+                        catch(exc)
+                        {
+                            console.error("dropdown: action(", text, ",", value, ") threw error:", exc);
+                        } // end try
+                    };
+                } // end if
+
+                element.dropdown(options);
+            } // end updateCallbacks
+
+            scope.$watch('dropdownAction', updateCallbacks);
+
+            scope.$watchCollection('dropdownOptions', function()
+            {
+                options = scope.dropdownOptions ? _.clone(scope.dropdownOptions) : {};
+                updateCallbacks();
+            });
         } // end link
 
         return {
             restrict: 'E',
             scope: {
-                dropdownOptions: '='
+                dropdownOptions: '=',
+                dropdownAction: '&'
             },
             link: link
         };
-    });
+    }]);
