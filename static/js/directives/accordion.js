996b6cd66377a57768ff9a73eed2a7eba943fce0
David H. Bronke <whitelynx@gmail.com>
2015-07-07 16:09:41 -0500

    Added support for `onOpen` and `onClose` handlers, and added the `accordionOpen` attribute.

--- static/js/directives/accordion.js
+++ static/js/directives/accordion.js
@@ -11,10 +11,33 @@ angular.module('webPGQ.directives')
 
             if(element.parents('.ui.accordion').length === 0)
             {
+                var accordionOpen = scope.accordionOpen || false;
                 element
                     .addClass('ui')
                     .accordion(_.defaults(
                         {
+                            onOpen: function()
+                            {
+                                accordionOpen = true;
+                                scope.accordionOpen = true;
+                                scope.$apply();
+
+                                if((scope.accordionOptions || {}).onOpen)
+                                {
+                                    return (scope.accordionOptions || {}).onOpen.apply(this, arguments);
+                                } // end if
+                            },
+                            onClose: function()
+                            {
+                                accordionOpen = false;
+                                scope.accordionOpen = false;
+                                scope.$apply();
+
+                                if((scope.accordionOptions || {}).onClose)
+                                {
+                                    return (scope.accordionOptions || {}).onClose.apply(this, arguments);
+                                } // end if
+                            },
                             onChange: function()
                             {
                                 scope.$emit('contentResized');
@@ -28,12 +51,22 @@ angular.module('webPGQ.directives')
                         scope.accordionOptions,
                         { exclusive: false } // By default, allow multiple sibling sections to be open at the same time.
                     ));
+
+                scope.$watch('accordionOpen', function(isOpen)
+                {
+                    if(accordionOpen != isOpen)
+                    {
+                        accordionOpen = isOpen;
+                        element.accordion(isOpen ? 'open' : 'close', 0);
+                    } // end if
+                });
             } // end if
         } // end link
 
         return {
             restrict: 'E',
             scope: {
+                accordionOpen: '=?',
                 accordionOptions: '='
             },
             link: link
